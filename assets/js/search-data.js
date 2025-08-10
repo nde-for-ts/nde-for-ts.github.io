bb39fd30c556c998f4651acf08786cddd1d41d8c
yongkyung-oh <yongkyung.oh@outlook.com>
2025-08-10 08:48:21 +0000

    Deploying to gh-pages from @ nde-for-ts/nde-for-ts.github.io@c53e8e58ff28b34f4abc1b7ee087fddde345a840 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-tutorial",
-    title: "Tutorial",
+    id: "nav-review",
+    title: "Review",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/review/";
+            window.location.href = "/";
           },
         },{id: "nav-foundational-papers",
           title: "Foundational Papers",
